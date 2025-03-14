4f10047a73e2b3e3215b8df9252d822a98e6efe6
Anmol Gupta (Student) <agupt374@en4217384l.fulton.ad.asu.edu>
2025-03-13 18:05:30 -0700

    edited versions

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 core_requirements = [
     "gymnasium==0.29.1",
-    "rich==13.7.1",
-    "tqdm==4.66.4",
-    "ipdb==0.13.13",
+    "rich",
+    "tqdm",
+    "ipdb",
     "mujoco==3.1.6",
     "mujoco-mjx==3.1.6",
     "dm_control==1.0.20",
-    "imageio==2.34.2",
-    "gymnax==0.0.8",
-    "brax==0.9.4",
-    "torch==2.3.1",
-    "opencv-python==4.10.0.84",
-    "natsort==8.4.0",
+    "imageio",
+    "gymnax",
+    "brax",
+    "torch",
+    "opencv-python",
+    "natsort",
 ]
 
 setup(
